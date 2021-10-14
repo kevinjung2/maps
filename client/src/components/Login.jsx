ad237aed071366a99b4bf028bc026310590fb5f4
Kevin Jung <kev.jung.ii@gmail.com>
2021-10-14 15:34:34 -0500

    adds username and password fields to login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FormGroup } from 'react-bootstrap';
+
+import Input from './Input'
 
 const Login = () => {
+  const [username, setUsername] = ""
+  const [password, setPassword] = ""
+
   return(
-    <h1>Login Page</h1>
+    <div className="login-form">
+      <h1 className="form__title">Login</h1>
+      <h2 className="form__desc">
+        Please login with your username and password.
+      </h2>
+      <h2 className="form__desc">
+        <span style={{ color: "red" }}>*</span> = required
+      </h2>
+      <FormGroup>
+        <div className="form-group col-md-8">
+          <Input
+            className={"required"}
+            type={"text"}
+            title={"Username"}
+            name={"username"}
+            value={username}
+            placeholder={"Username"}
+            handleChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group col-md-8">
+          <Input
+            className={"required"}
+            type={"password"}
+            title={"Password"}
+            name={"password"}
+            value={password}
+            placeholder={"Password"}
+            handleChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </FormGroup>
+    </div>
   )
 }
 
